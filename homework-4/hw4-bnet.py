6b2ea35f611325a95611e8b7ede40de2c9dd1839
Natalia <ndibbern@lion.lmu.edu>
2018-04-06 18:46:40 -0700

    finish second part of HW

--- homework-4/hw4-bnet.py
+++ homework-4/hw4-bnet.py
@@ -1,34 +1,23 @@
 from pomegranate import *
 
-age = DiscreteDistribution( { '0':0.5, '1':0.5 } )
-political_inclination = DiscreteDistribution( { '0':0.5, '1':0.5 } )
+a = DiscreteDistribution( { '0':0.5, '1':0.5 } )
+p = DiscreteDistribution( { '0':0.5, '1':0.5 } )
 
-gun = ConditionalProbabilityTable(
+g = ConditionalProbabilityTable(
         [[ '0', '0', 0.3 ],
          [ '0', '1', 0.7 ],
          [ '1', '0', 0.6 ],
-         [ '1', '1', 0.4 ],
-         [age] )
+         [ '1', '1', 0.4 ]],
+         [a] )
 
-immigration = ConditionalProbabilityTable(
+i = ConditionalProbabilityTable(
         [[ '0', '0', 0.25 ],
          [ '0', '1', 0.75 ],
          [ '1', '0', 0.20 ],
-         [ '1', '1', 0.80 ],
-         [age] )
+         [ '1', '1', 0.80 ]],
+         [p] )
 
-voting = ConditionalProbabilityTable(
-        [[ '0', '0', '0', 0.3 ],
-         [ '0', '1', '0', 0.5 ],
-         [ '1', '0', '0', 0.8 ],
-         [ '1', '1', '0', 0.6 ],
-         [ '0', '0', '1', 0.7 ],
-         [ '0', '1', '1', 0.5 ],
-         [ '1', '0', '1', 0.2 ],
-         [ '1', '1', '1', 0.4 ]],
-         [gun, immigration] )
-
-drug = ConditionalProbabilityTable(
+d = ConditionalProbabilityTable(
         [[ '0', '0', '0', 0.45 ],
          [ '0', '1', '0', 0.75 ],
          [ '1', '0', '0', 0.30 ],
@@ -37,4 +26,86 @@ drug = ConditionalProbabilityTable(
          [ '0', '1', '1', 0.25 ],
          [ '1', '0', '1', 0.70 ],
          [ '1', '1', '1', 0.85 ]],
-         [political_inclination, age] )
+         [p, a] )
+
+v = ConditionalProbabilityTable(
+        [[ '0', '0', '0', 0.3 ],
+         [ '0', '1', '0', 0.5 ],
+         [ '1', '0', '0', 0.8 ],
+         [ '1', '1', '0', 0.6 ],
+         [ '0', '0', '1', 0.7 ],
+         [ '0', '1', '1', 0.5 ],
+         [ '1', '0', '1', 0.2 ],
+         [ '1', '1', '1', 0.4 ]],
+         [g, i] )
+
+s_a = State( a, name="A" )
+s_p = State( p, name="P" )
+s_i = State( i, name="I" )
+s_g = State( g, name="G" )
+s_d = State( d, name="D" )
+s_v = State( v, name="V" )
+
+model = BayesianNetwork("Elections")
+
+model.add_states(s_a, s_p, s_i, s_g, s_d, s_v)
+
+model.add_transition(s_g, s_v)
+model.add_transition(s_i, s_v)
+model.add_transition(s_p, s_i)
+model.add_transition(s_p, s_d)
+model.add_transition(s_a, s_g)
+model.add_transition(s_a, s_d)
+
+model.bake()
+
+v_distribution = model.predict_proba({})[-1]
+v_probabilities = v_distribution.parameters[0]
+
+print(v_probabilities['1'])
+# 1) My oponent is charted to win the congressional seat
+print(v_probabilities['0'] - v_probabilities['1'] )
+# by a margin of ~12%
+
+print('\nPart 2\n')
+
+'''
+Second, for each of the following,
+(a) compute the probability requested and then
+(b) determine if should target the individual for canvassing (when oponent wins '0' > '1')
+(c) record their values in your report.
+'''
+# oponent wins -> target
+v_distribution = model.predict_proba({'A':'1'})[-1]
+v_probabilities = v_distribution.parameters[0]
+print(v_probabilities)
+
+# I win -> DONT target
+v_distribution = model.predict_proba({'G':'0'})[-1]
+v_probabilities = v_distribution.parameters[0]
+print(v_probabilities)
+
+# I win -> DONT target
+v_distribution = model.predict_proba({'G':'0','I':'0'})[-1]
+v_probabilities = v_distribution.parameters[0]
+print(v_probabilities)
+
+# oponent wins -> target
+v_distribution = model.predict_proba({'I':'0','G':'1'})[-1]
+v_probabilities = v_distribution.parameters[0]
+print(v_probabilities)
+
+# I win -> DONT target
+v_distribution = model.predict_proba({'A':'0','P':'1','I':'0','D':'0','G':'0'})[-1]
+v_probabilities = v_distribution.parameters[0]
+print(v_probabilities)
+
+# I ALMOST WIN -> target
+v_distribution = model.predict_proba({'A':'0','P':'1','I':'1','D':'1','G':'0'})[-1]
+v_probabilities = v_distribution.parameters[0]
+print(v_probabilities)
+
+# oponent wins -> target
+v_distribution = model.predict_proba({'A':'1','P':'0','I':'0','D':'0','G':'1'})[-1]
+v_probabilities = v_distribution.parameters[0]
+print(v_probabilities)
---
+++ homework-4/report.md
@@ -0,0 +1,46 @@
+# AI- HW 4:
+
+Search algorithm graph:
+
+![Screen Shot 2018-04-06 at 5.17.15 PM](/Users/ndibbern/Desktop/Screen Shot 2018-04-06 at 5.17.15 PM.png)
+
+Graph after I fix the undirected edges:
+
+
+
+![Screen Shot 2018-04-06 at 5.16.07 PM](/Users/ndibbern/Desktop/Screen Shot 2018-04-06 at 5.16.07 PM.png)
+
+#### CPT for D:
+
+![Screen Shot 2018-04-06 at 5.16.13 PM](/Users/ndibbern/Desktop/Screen Shot 2018-04-06 at 5.16.13 PM.png)
+
+
+
+#### CPT for A (just probability table):
+
+![Screen Shot 2018-04-06 at 5.16.34 PM](/Users/ndibbern/Desktop/Screen Shot 2018-04-06 at 5.16.34 PM.png)
+
+
+
+#### CPT for G:
+
+![Screen Shot 2018-04-06 at 5.16.40 PM](/Users/ndibbern/Desktop/Screen Shot 2018-04-06 at 5.16.40 PM.png)
+
+
+
+#### CPT for I:
+
+![Screen Shot 2018-04-06 at 5.16.46 PM](/Users/ndibbern/Desktop/Screen Shot 2018-04-06 at 5.16.46 PM.png)
+
+
+
+#### CPT for P (just probability table):
+
+![Screen Shot 2018-04-06 at 5.16.52 PM](/Users/ndibbern/Desktop/Screen Shot 2018-04-06 at 5.16.52 PM.png)
+
+
+
+#### CPT for V:
+
+![Screen Shot 2018-04-06 at 5.16.56 PM](/Users/ndibbern/Desktop/Screen Shot 2018-04-06 at 5.16.56 PM.png)
+
